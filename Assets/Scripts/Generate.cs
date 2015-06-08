c04c48de0381a47f572d317e45367b61c56afa71
Shreyaansh Bassi <shreyaanshbassi@gmail.com>
2015-06-09 02:43:50 +0530

    Creation of the game
    Yay, Created my first game

---
+++ Assets/Scripts/Generate.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Generate : MonoBehaviour {
+
+	// Use this for initialization
+	public GameObject rocks;
+	void Start () {
+		InvokeRepeating ("CreateObstacle", 1.5f, 1.5f);
+	}
+
+	void CreateObstacle () {
+		Instantiate (rocks);
+	}
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+	// Use this for initialization
+	public Vector2 jumpForce = new Vector2(0, 300);
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyUp ("space")) {
+			rigidbody2D.velocity = Vector2.zero;
+			rigidbody2D.AddForce(jumpForce);
+			//yield return new WaitForSeconds(1.5f);
+		}
+
+		Vector2 screenPosition = Camera.main.WorldToScreenPoint(transform.position);
+		if (screenPosition.y > Screen.height || screenPosition.y < 0) {
+			//yield return new WaitForSeconds (1.5f);
+			Die();
+		}
+		//yield return new WaitForSeconds(1.5f);
+	}
+
+	IEnumerator OnCollisionEnter2D (Collision2D other) {
+		yield return new WaitForSeconds(1.5f);
+		Die ();
+	}
+	void Die () {
+		Application.LoadLevel (Application.loadedLevel);
+	}
+}
